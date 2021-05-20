3a02775b423aff877e7bc86084793d6a02aa423f
imintsao <s3104029@gmail.com>
2021-05-20 23:26:28 +0800

    Graph grid commit

--- Assets/Scripts/FunctionLibraryImin.cs
+++ Assets/Scripts/FunctionLibraryImin.cs
@@ -6,7 +6,7 @@ public static class FunctionLibraryImin //ok,it is "static". so it let us
     // to use
 
 {
-    public static float Wave(float x, float t)//ok,public + static, 
+    public static float Wave(float x, float z, float t)//ok,public + static, 
         // float means it can produce a result
         //put float into parameter list, so it will be just like for the
         //mathematical function.
@@ -15,7 +15,31 @@ public static class FunctionLibraryImin //ok,it is "static". so it let us
         // now we are inside the method!
         // "return" to return a float value
     }
-    public static float MultiWave (float x, float t)//A Second Function
+
+    public delegate float Function(float x, float z, float t);//a method signature
+                                                     //without an implementation
+    public enum FunctionName {Wave, MultiWave, Ripple }
+
+    static Function[] functions = { Wave, MultiWave, Ripple };
+
+    public static Function GetFunction (FunctionName name)
+    {
+        return functions[(int)name];
+        //if (index == 0)
+        //{
+        //    return Wave;
+        //}
+        //else if (index == 1)
+        //{
+        //    return MultiWave;
+        //}
+        //else
+        //{
+        //    return Ripple;
+        //}
+    }
+
+    public static float MultiWave (float x, float z, float t)//A Second Function
     {
         //return Sin(PI * (x + t));
         //float y = Sin(PI * (x + t));
@@ -24,11 +48,16 @@ public static class FunctionLibraryImin //ok,it is "static". so it let us
         y += 0.5f * Sin(2f * PI * (x + t));
         return y / 1.5f; 
     }
-    public static float Ripple (float x, float t)
+    public static float Ripple (float x, float z, float t)
     {
         float d = Abs(x);
-        float y = Sin(PI * (4f * d -t));
+        float y = Sin(PI * (4f * d - t));  //final result
         return y / (1f + 10f * d);
+
+        //float y = Sin(4f * PI * d);
+        //return y;
+
+        
     }
 
 }
--- Assets/Scripts/GraphImin.cs
+++ Assets/Scripts/GraphImin.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -11,8 +11,10 @@ public class GraphImin : MonoBehaviour
     [SerializeField, Range(10, 100)]
     int resolution = 10;
 
-    [SerializeField, Range(0, 2)]
-    int function;
+    //[SerializeField, Range(0, 2)]
+    [SerializeField]
+
+    FunctionLibraryImin.FunctionName function;
 
     Transform[] points; // add points field, and turn field into an array
 
@@ -27,16 +29,22 @@ public class GraphImin : MonoBehaviour
         //var scale = Vector3.one / 5f;
         var scale = Vector3.one * step;
         var position = Vector3.zero;
-        points = new Transform [resolution];
-        for (int i = 0; i<points.Length; i++) { 
-            Transform point = Instantiate(pointPrefab);
+        points = new Transform [resolution * resolution];
+        for (int i = 0, x = 0, z = 0; i < points.Length; i++, x++) {
+            if (x == resolution)
+            {
+                x = 0;
+                z += 1;
+            }
+            Transform point = points[i] = Instantiate(pointPrefab);
             //point.localPosition = Vector3.right * i;
             //point.localPosition = Vector3.right * i / 5f;
             //point.localPosition = Vector3.right * ((i + 0.5f) / 5f - 1f);
             //point.localScale = Vector3.one / 5f;
             //position.x = (i + 0.5f) / 5f - 1f;
-            position.x = (i + 0.5f) * step - 1f;
-            //position.y = position.x; //(becomes like a stair)
+            position.x = (x + 0.5f) * step - 1f;
+            position.z = (z + 0.5f) * step - 1f;
+                //position.y = position.x; //(becomes like a stair)
             //position.y = position.x * position.x; //(becomes like a curve)
             position.y = position.x * position.x * position.x; //why using Y?
             point.localPosition = position;
@@ -54,6 +62,7 @@ public class GraphImin : MonoBehaviour
 
     private void Update()
     {
+        FunctionLibraryImin.Function f = FunctionLibraryImin.GetFunction(function);
         float time = Time.time;
         for (int i = 0; i < points.Length; i++)
         {
@@ -63,19 +72,20 @@ public class GraphImin : MonoBehaviour
             //position.y = Mathf.Sin(Mathf.PI * (position.x + time));
             //position.y = FunctionLibraryImin.Wave(position.x, time);
             //it's now possible to invoke this method inside "Graph.Update"
-            if (function == 0)//We have "if" now!herai herai!
-            {
-                position.y = FunctionLibraryImin.Wave(position.x, time);
-            }
-            else if (function == 1)
-            {
-                position.y = FunctionLibraryImin.MultiWave(position.x, time);
-            }
-            else
-            {
-                position.y = FunctionLibraryImin.Ripple(position.x, time);
 
-            }
+            //if (function == 0)
+            //{
+            //    position.y = FunctionLibraryImin.Wave(position.x, time);
+            //}
+            //else if (function == 1)
+            //{
+            //    position.y = FunctionLibraryImin.MultiWave(position.x, time);
+            //}
+            //else
+            //{
+            //    position.y = FunctionLibraryImin.Ripple(position.x, time);
+            //}
+            position.y = f(position.x, position.z, time);
             point.localPosition = position;
         }
     }
